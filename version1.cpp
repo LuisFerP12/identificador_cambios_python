fde8fd078439c3141543a25e7967232860a2c516
LuisFerP12 <102192932+LuisFerP12@users.noreply.github.com>
2024-04-18 00:49:44 -0600

    Lineas ajustadas: Salto de linea y coloreo completo de columna, y modificacion scrollbar

--- version1.cpp
+++ version1.cpp
@@ -3,7 +3,7 @@
  * Copyright 2018 The diff-match-patch Authors.
  * https://github.com/google/diff-match-patch
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache Licenciado, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -11,14 +11,14 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
 #include <algorithm>
 #include <limits>
-// Code known to compile and run with Qt 4.3 through Qt 4.7.
+// Code known to compile and runn with Qt 4.3 through Qt 4.7.
 #include <QtCore>
 #include <time.h>
 #include "diff_match_patch.h"
